c85a034e4af6eddad1b4a40892404066d8863c8d
Michael Thies and Tom Kersten <devs+mike+tom@codefumes.com>
2010-04-21 14:56:43 -0500

    Version bump to 0.1.0

--- lib/build_watcher.rb
+++ lib/build_watcher.rb
@@ -6,7 +6,7 @@ require 'build_watcher/message'
 require 'serialport'
 
 module BuildWatcher
-  VERSION = '0.0.1'
+  VERSION = '0.1.0'
 
   class AppropriateMessageTypeNotFound < StandardError; end
 end
